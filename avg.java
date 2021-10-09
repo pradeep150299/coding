62117c398a7f83b7b855b1ac5fcf20c7cda27bcf
pradeep150299 <92204948+pradeep150299@users.noreply.github.com>
2021-10-09 15:18:04 +0530

    nested if

---
+++ avg.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class avg{
+	public static void main(String[] args)
+	{
+	Scanner input=new Scanner(System.in);
+	int a=input.nextInt();
+	int b=input.nextInt();
+	if(a<b)
+	{
+		System.out.println("a is small");
+	}
+	if(b<a)
+	{
+		System.out.println("b is small");
+	}
+	}
+
+} 
